8753675f62d8755df3fcb15d81ebf293ddbde815
Роман <vodaz@mail.ru>
2020-05-21 16:00:10 +0300

    Initial commit

---
+++ src/main/java/app/model/Cat.java
@@ -0,0 +1,14 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("Cat")
+public class Cat extends Animal {
+    @Override
+    public String toString() {
+        return "Im a Cat";
+    }
+}
